915f28734ef3c70945932a4df89a15a279dd68fc
Khaled Javedan <khaledjavedan76@gmail.com>
2022-10-08 09:28:19 +0330

    working on the pageRoute component

--- src/components/styles/PageRoute.js
+++ src/components/styles/PageRoute.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { StyledPageRoute } from './PageRoute.styled'
+import {GoHome} from 'react-icons/go'
+import '../../assets/App.css'
+import {BiChevronRight} from 'react-icons/bi'
 
 const PageRoute = () => {
   return (
     <StyledPageRoute>
-        
+        <div className="root"> 
+        <GoHome /> <span>Home</span> <BiChevronRight /> <span>Mobile</span> <BiChevronRight /> <span>Apple</span>
+        </div>
     </StyledPageRoute>
   )
 }
--- src/components/styles/PageRoute.styled.js
+++ src/components/styles/PageRoute.styled.js
@@ -3,5 +3,4 @@ import styled from 'styled-components'
 export const StyledPageRoute = styled.div`
 width: 65%;
 height: 50px;
-background-color: red;
 `
