337ef2a8354136d097047f72477907a18a1b4cb4
Khaled Javedan <khaledjavedan76@gmail.com>
2022-10-13 21:17:36 +0330

    working on costum checkbox

--- src/layouts/sidebar/components/Checkbox.js
+++ src/layouts/sidebar/components/Checkbox.js
@@ -4,8 +4,7 @@ import { StyledCheckbox, Label , Input } from '../styles/Checkbox.styled'
 const Checkbox = () => {
   return (
     <StyledCheckbox>
-      <Input id='cb1'/>
-      <Label htmlFor='cb1'>checkbox</Label>
+      
     </StyledCheckbox>
   )
 }
