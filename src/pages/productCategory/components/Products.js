a3664ba684fb9c2afca993098b3a7b4b5e984396
khaled javedan <khaledjavedan76@gmail.com>
2022-11-10 16:54:55 +0400

    fixing responsiveness  problem in small screen

--- src/pages/productCategory/components/Products.js
+++ src/pages/productCategory/components/Products.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ProductsContainer from '../styles/Products.styled'
+import Product from '../../../components/Product'
 
 const Products = () => {
   return (
     <ProductsContainer>
-
+        <Product />
+        <Product />
     </ProductsContainer>
   )
 }
