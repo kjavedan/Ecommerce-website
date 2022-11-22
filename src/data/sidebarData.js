46722005969e893e0941bce54732db728357aaa7
khaled javedan <khaledjavedan76@gmail.com>
2022-11-22 10:43:16 +0400

    working on the category sidebar filters

--- src/data/sidebarData.js
+++ src/data/sidebarData.js
@@ -1,10 +1,13 @@
-import { HiOutlineDeviceMobile, HiStar, HiArrowSmDown, HiArrowSmUp } from 'react-icons/hi'
-import {BsLaptop, BsApple} from 'react-icons/bs'
-import {CgMouse} from 'react-icons/cg'
+import { HiOutlineDeviceMobile, HiStar, HiArrowSmDown, HiArrowSmUp, HiCamera } from 'react-icons/hi'
+import {BsLaptop, BsApple, BsArrowsFullscreen} from 'react-icons/bs'
+import {CgMouse, CgSmartphoneRam} from 'react-icons/cg'
 import {FaRegKeyboard} from 'react-icons/fa'
 import {FiHeadphones, FiCpu} from 'react-icons/fi'
-import {AiFillHeart, AiFillFire} from 'react-icons/ai'
-import {MdShoppingCart, MdLocalOffer} from 'react-icons/md'
+import {AiFillHeart, AiFillFire, AiOutlineFullscreen} from 'react-icons/ai'
+import {MdShoppingCart, MdLocalOffer, MdColorLens} from 'react-icons/md'
+import {VscColorMode} from 'react-icons/vsc'
+import {RiCameraLensFill} from 'react-icons/ri'
+import { BiMemoryCard } from 'react-icons/bi'
 
 export const sidebarDataHome = [
     {
@@ -80,8 +83,38 @@ export const sidebarDataMobile = [
         data: [
         {
           title: 'Brands',
-          icon: <BsApple styled={{color: 'grey'} } />,
+          icon: <BsApple />,
           children: ['all', 'apple', 'samsung', 'sony', 'howavei', 'shiami']
+        },
+        {
+          title: 'Colors',
+          icon: <VscColorMode  />,
+          children: ['black', 'white', 'blue', 'red', 'green', 'gray']
+        },
+        {
+          title: 'Camera',
+          icon: <HiCamera  />,
+          children: ['0 comera',' 1 camera', '2 comera', '3 comera']
+        },
+        {
+          title: 'resolution',
+          icon: <RiCameraLensFill  />,
+          children: ['5MP', '8MP', '12MP', '20MP', '40MP', '48MP']
+        },
+        {
+          title: 'RAM',
+          icon: <CgSmartphoneRam  />,
+          children: ['2GB', '4GB', '6GB', '8GB', '16GB', '32GB']
+        },
+        {
+          title: 'Memory',
+          icon: <BiMemoryCard  />,
+          children: ['8GB', '16GB', '32GB', '64GB', '256GB', '512GB', '1TB']
+        },
+        {
+          title: 'Screen Size',
+          icon: <AiOutlineFullscreen  />,
+          children: ['3 to 4 inch', '3 to 5 inch', '5 to 6 inch', '6 to 7 inch', '13 to 14 inch', '14 to 17 inch', '> 17 inch']
         },
         // {
         //   title: 'most sailed',
