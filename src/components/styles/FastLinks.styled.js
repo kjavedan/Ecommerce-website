d9e64e4a3aed98ca690ff4fd5a28bc68e4e09001
khaled javedan <khaledjavedan76@gmail.com>
2022-11-23 14:14:40 +0400

    changed the fast links h4 style

--- src/components/styles/FastLinks.styled.js
+++ src/components/styles/FastLinks.styled.js
@@ -7,6 +7,11 @@ export const StyledFastLinks = styled.div`
     padding-left: 1rem;
     color: ${({theme}) => theme === '#fff' ? '#000' : '#B4B4B4'};
     margin-bottom: .7rem;
+
+    h4{
+        width: 80px;
+        background-color: red;
+    }
     
     @media (min-width: 900px){
         padding-left: 3.3rem;
